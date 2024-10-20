d700afe8acb2a3ff00b4a99e2051b93f6b68e58b
rawrrr <nina.lepage.25@gmail.com>
2024-10-20 12:46:32 -0400

    finished the assignment

--- assignment1.py
+++ assignment1.py
@@ -9,14 +9,29 @@ mid2 = float(input("Enter grade for Midterm 2: "))
 fin = float(input("Enter grade for Final Exam: "))
 midfin = float(input("Enter grade for Midterms and Final Preparation: "))
 
-grade = ((labs*((100/6)*20)/100) + 
-            (quizzes*((100/6)*15)/100) + 
-            ((as1*4)/100) +
-            ((as1*4)/100) +
-            ((as3*4)/100) +
-            ((as4*4)/100) +
-            ((mid1*12.5)/100) +
-            ((mid2*12.5)/100) +
-            ((fin*18)/100) +
-            ((midfin*6)/100))
+
+if quizzes > 6:
+    quizzes = 6.0
+else:
+    quizzes = quizzes
+
+if labs > 6:
+    labs = 6.0
+else:
+    labs = labs
+
+def grade_calculation(labs, quizzes, as1, as2, as3, as4, mid1, mid2, fin, midfin):
+    return (((labs)*((100/6)*20)/100) + 
+    ((quizzes)*((100/6)*15)/100) + 
+    ((as1*4)/100) +
+    ((as1*4)/100) +
+    ((as3*4)/100) +
+    ((as4*4)/100) +
+    ((mid1*12.5)/100) +
+    ((mid2*12.5)/100) +
+    ((fin*18)/100) +
+    ((midfin*6)/100))
+
+grade = grade_calculation(labs, quizzes, as1, as2, as3, as4, mid1, mid2, fin, midfin)
+
 print("Your garde is: " + str(round(grade)))
